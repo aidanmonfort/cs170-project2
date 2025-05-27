92d3b29029839cb1565c8232f26865ff7fe826e4
aidanmonfort <aidanmonfort1271@gmail.com>
2025-05-27 14:07:49 -0700

    finished the backward elimanation, pretty much the same code

--- part-one/main.py
+++ part-one/main.py
@@ -23,6 +23,7 @@ def forward_selection(feature_count):
         if not possible_features:#if we have taken all possible features
             break
 
+        #creates a tuple list of all subsets with one feature added, and scores the accuracy randomly
         next_take = [(random_eval(), [pf] + feature_set) for pf in possible_features]
         
         for accuracy, subset in next_take:
@@ -36,8 +37,9 @@ def forward_selection(feature_count):
             print(f"Feature set {{ {','.join(str(f) for f in best_subset)} }} was best,",
                   f"accuracy is {best_accuracy: .3f}%")
             current_accuracy = best_accuracy
-            feature_set = best_subset
+            feature_set = best_subset#update to best feature set found
             remove_feature = best_subset[0]
+            #remove the chosen feature from the list of features we can add
             possible_features.remove(remove_feature)
 
     print(f"Search finished! The best subset of features is {{ {','.join(str(f) for f in feature_set)} }},",
@@ -45,8 +47,35 @@ def forward_selection(feature_count):
 
 
 
-def backward_elimination():
-    pass
+def backward_elimination(feature_count):
+    start_features = set(range(1, feature_count + 1))
+    current_accuracy = random_eval()
+
+    print("Using all features and “random” evaluation, I get an accuracy of:", f"{current_accuracy: .3f}%")
+
+    while True:
+        if not start_features:
+            break
+
+        #creates a tuple list of all subsets with one feature removed, and scores the accuracy randomly
+        possible_removes = [(random_eval(), start_features - {feature}) for feature in start_features]
+        for accuracy, subset in possible_removes:
+            print(f"\tUsing feature(s) {{ {','.join(str(f) for f in subset)} }}, accuracy is {accuracy: .3f}")
+
+        #will grab the best new feature subset using tuple comparison
+        best_accuracy, best_subset = max(possible_removes)
+
+        if best_accuracy < current_accuracy:
+            print("(Warning: Decreased accuracy! )")
+            break
+        else:
+            print(f"Feature set {{ {','.join(str(f) for f in best_subset)} }} was best,",
+                  f"accuracy is {best_accuracy: .3f}%")
+            current_accuracy = best_accuracy
+            start_features = best_subset#update to new best set
+
+    print(f"Search finished! The best subset of features is {{ {','.join(str(f) for f in start_features)} }},",
+          f"which has an accuracy of {current_accuracy: .3f}%")
 
 def main():
     print("Welcome to Aidan Monfort and Shaun Mansoor", 
